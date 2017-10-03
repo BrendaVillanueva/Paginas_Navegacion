44950f7f88d4bac107b5d2ec6a17e002dc7858ab
BRENDA <BRENDA@DESKTOP-7O4U7A5>
2017-10-02 20:03:31 -0500

    Agregar archivos de proyecto.

---
+++ Paginas_Navegacion/Paginas_Navegacion/Datos.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Paginas_Navegacion
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Datos : ContentPage
+    {
+        public Datos()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Formulario());
+        }
+    }
+}
---
+++ Paginas_Navegacion/Paginas_Navegacion/Formulario.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Paginas_Navegacion
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Formulario : ContentPage
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+        }
+
+        private void Enviar_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Datos Ingresados", Nombre.Text + " " + Apellido.Text + "\n " + Direccion.Text + " \n " + TEL.Text + " \n" + Carrera.Text + " \n" + Semestre.Text + "\n " + Matricula.Text + "\n " + Correo.Text + "\n " + GitHub.Text, "OK");
+            Navigation.PushAsync(new Calculadora());
+        }
+    }
+}
